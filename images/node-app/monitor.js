1fa1a4dcdbba885eb130b164505fc9c9bf7da666
Darlan Alves <me@darlanalv.es>
2020-09-15 15:21:01 +0200

    fix: pass correct port to superstatic

--- images/node-app/monitor.js
+++ images/node-app/monitor.js
@@ -49,7 +49,7 @@ function getCommand() {
   const staticsJson = Path.join(process.cwd(), 'superstatic.json');
 
   if (FS.existsSync(staticsJson)) {
-    return { command: 'superstatic', args: ['--port', '$PORT'] };
+    return { command: 'superstatic', args: ['--port', process.env.PORT] };
   }
 
   if (FS.existsSync(packageJson)) {
